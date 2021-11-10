ad470194357d5c9dc9ae3297193940af4019ee8e
psw <parksw125368@naver.com>
2021-11-10 20:57:53 +0900

    Project11_211110_2

--- 11_Alarm/app/src/main/java/com/example/a11_alarm/MainActivity.kt
+++ 11_Alarm/app/src/main/java/com/example/a11_alarm/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.a11_alarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //step0 뷰를 초기화 해주기
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+        //step1 데이터 가져오기
+        //step2 가져온 데이터를 view에 그려주기
+        //step3
+    }
+    private fun initOnOffButton(){
+        val onOffButton = findViewById<Button>(R.id.onOffButton)
+        onOffButton.setOnClickListener {
+            // 데이터를 확인한다.
+
+            // 온 오프에 따라 작업을 처리한다.
+            // 온 -> 알람 등록
+            // 오프 -> 알람제거
+        }
+
+    }
+
+    private fun initChangeAlarmTimeButton(){
+
     }
 }
