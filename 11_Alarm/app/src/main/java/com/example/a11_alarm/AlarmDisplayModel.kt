1eae428d937e9850b05df8cde9d87e728bf0d40e
CookDas2 <parksw125368@naver.com>
2021-11-11 01:49:41 +0900

    Projcet11_211111_1

---
+++ 11_Alarm/app/src/main/java/com/example/a11_alarm/AlarmDisplayModel.kt
@@ -0,0 +1,19 @@
+package com.example.a11_alarm
+
+data class AlarmDisplayModel(
+    val hour : Int,
+    val minute : Int,
+    var onOff : Boolean
+){
+    val tiemText : String
+        get() {
+            val h = "%02d".format( if(hour<12) hour else hour-12)
+            val m = "%02".format(minute)
+
+            return "$h:$m"
+        }
+    val ampmText : String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+}
--- 11_Alarm/app/src/main/java/com/example/a11_alarm/MainActivity.kt
+++ 11_Alarm/app/src/main/java/com/example/a11_alarm/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.a11_alarm
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         val onOffButton = findViewById<Button>(R.id.onOffButton)
         onOffButton.setOnClickListener {
             // 데이터를 확인한다.
-
             // 온 오프에 따라 작업을 처리한다.
             // 온 -> 알람 등록
             // 오프 -> 알람제거
@@ -29,6 +30,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initChangeAlarmTimeButton(){
+        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
+        changeAlarmButton.setOnClickListener {
+            //현재시가 가져오기
+            val calender = Calendar.getInstance()//시스템에 적용된 현재 시간을 가져온다
 
+            //Time다이얼로그 띄워서 시간 지정후 종료할때 작업해주기
+            TimePickerDialog(this,
+                { picker, hour, minute -> //타임을 지정하고 나올때 발생하는 이벤트 람다.
+                    //1.데이터를 저장하고 2.View를 업데이트하고 3.기존에 있던 알람을 삭제하고
+                    val model = saveAlarmModel(hour, minute, false)
+                },
+                calender.get(Calendar.HOUR_OF_DAY) ,
+                calender.get(Calendar.MINUTE),
+                false
+            ).show()
+        }
+    }
+    private fun saveAlarmModel(
+        hour:Int,
+        minute:Int,
+        onOff : Boolean
+    ):AlarmDisplayModel{
+        val model = AlarmDisplayModel(
+            hour = hour,
+            minute = minute,
+            onOff = false
+        )
+        return model
     }
 }
